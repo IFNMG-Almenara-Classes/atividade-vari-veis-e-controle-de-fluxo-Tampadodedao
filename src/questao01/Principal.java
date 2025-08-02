9cd4739ebd085ee38d528cfa149f1b849293cf4c
Bryan2225 <matheusteualmeidaro@gmail.com>
2025-08-01 22:11:14 -0300

    Resolução da questão 1

--- src/questao01/Principal.java
+++ src/questao01/Principal.java
@@ -1,7 +1,23 @@
 package questao01;
 
-public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.Scanner;
+
+public class Principal{
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int x = entrada.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int y = entrada.nextInt();
+
+        System.out.println("O maior númro é: " + ((x>y) ? x : y));
     }
 }
+
+/**
+ * package questão01;
+ * 
+ * public class Principal{
+ *      public static void main(String[]args){
+ *          System.out.println("Hello World!");}}
+ */
