f04914baecb46007018b3ca6c210db56c69ee331
Bryan2225 <matheusteualmeidaro@gmail.com>
2025-08-01 22:25:24 -0300

    Resolução questão dois - falta terminá-la

---
+++ src/questao02/Principal.java
@@ -0,0 +1,23 @@
+package questao02;
+
+import java.util.Scanner;
+
+public class Principal{
+    public static void main(String []args){
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe o seu peso: ");
+        float peso = entrada.nextFloat();
+        System.out.println("Informe a sua altura: ");
+        float altura = entrada.nextFloat();
+
+        float iMC = peso/altura*altura;
+
+        if(iMC<18){}
+        if(iMC>18 && iMC<){}
+        if(iMC<18){}
+        if(iMC<18){}
+        if(iMC<18){}
+
+    }
+}
